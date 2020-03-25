b4d21702463eacf332e83f42d9e9c92fc0561852
daniel yung <zhidarong@gmail.com>
2020-03-25 15:52:49 +0800

    rebuilding site 2020年 3月25日 星期三 15时52分49秒 CST

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+# Build the project.
+hugo
+
+cd public
+
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+git subtree push --prefix=public git@github.com:rongzhida/rongzhida.github.io.git gh-pages
